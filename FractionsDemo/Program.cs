6abb67b5e724b6e11b370c23b0565f2d9cc961b6
sam-klok <sklok11@gmail.com>
2022-06-09 10:20:48 -0700

    Initial commit

---
+++ FractionsDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Fractions;
+
+var strF1 = "3/9"; // fraction to simplify
+var f1 = Fraction.FromString(strF1);
+Console.WriteLine($"Reducing fraction: {strF1} = {f1}");
+
+var f2 = new Fraction(4, 16, true);
+Console.WriteLine($"4/16 = {f2}");
+
+var f3 = f1 + f2;
+Console.WriteLine($"{f1} + {f2} = {f3}");
+
+
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Let's be lazy and use existhing math library for reducing, and adding fractions.
+Based on Fractions.dll from NuGet package "Fractions by Dainel Mueller".
+Thank you,
+Sam Klok
+
